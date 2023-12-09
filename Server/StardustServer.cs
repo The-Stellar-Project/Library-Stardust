34192f97fa177b9f870e5d58043b06b6625096d2
sugisaru <blank1337@keemail.me>
2023-12-09 17:43:43 +0100

    :sparkles: added develop branch :D

--- Server/StardustServer.cs
+++ Server/StardustServer.cs
@@ -1,6 +1,6 @@
 namespace Stardust.Server {
 	public class StardustServer {
-		public static void Run(in Dictionary<string, string> args) {
+		public static void Run(in Dictionary<string, string> args) { // hi
 			Console.WriteLine(value: "server detected");
 			foreach (var pair in args) Console.WriteLine(value: $"key: {pair.Key} ; value: {pair.Value}");
 		}
